14b8789a5801050a7678c99d77ce6e498f7703c9
leuwenhoek <ninjabeastyy24@gmail.com>
2026-06-13 03:35:39 +0530

    UPDATE: RAG.py

--- RAG.py
+++ RAG.py
@@ -1,4 +1,7 @@
+# Welcome to a Human written code :)
+
 import os
+
 
 # Returns the list of file inside the folder (return type : list)
 def load_data(dir_location):
@@ -9,10 +12,10 @@ def main():
     Voyager1_Files = os.path.join('RAG data','Voyager 1')
     Voyager2_Files = os.path.join('RAG data','Voyager 2')
 
-    Voyager1_Data = load_data(Voyager1_Files)           # list
-    Voyager2_Data = load_data(Voyager2_Files)           # list
+    Voyager1_Data = load_data(Voyager1_Files)           # Data is in list
+    Voyager2_Data = load_data(Voyager2_Files)           # Data is in list
 
-    
+
 
     return 0
 
