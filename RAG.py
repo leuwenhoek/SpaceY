6c57043ec9e5d8fd7406a014226b426c1725aadc
leuwenhoek <ninjabeastyy24@gmail.com>
2026-06-13 01:20:21 +0530

    Loading the Data of Voyager 1 & 2

---
+++ RAG.py
@@ -0,0 +1,20 @@
+import os
+
+# Returns the list of file inside the folder (return type : list)
+def load_data(dir_location):
+    file_list = os.listdir(dir_location)
+    return file_list
+
+def main():
+    Voyager1_Files = os.path.join('RAG data','Voyager 1')
+    Voyager2_Files = os.path.join('RAG data','Voyager 2')
+
+    Voyager1_Data = load_data(Voyager1_Files)           # list
+    Voyager2_Data = load_data(Voyager2_Files)           # list
+
+    
+
+    return 0
+
+if __name__ == '__main__':
+    main()        
